a1407b92e5068397cb80b3e831c205bba71000c4
timo <timo.buehlmann@gmail.com>
2017-11-24 13:03:42 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,37 @@ public class Main {
     public static void main(String[] args) {
         Sequence firstSequence = new Sequence(10);
         Sequence secondSequence = new Sequence(1, 2, 3);
+        Sequence thirdSequence = new Sequence(1, 2, 3, 10, 0, 0);
+        Sequence fourthSequence = new Sequence(2, 10, 0, 0, 1, 3);
+        Sequence fifthSequence = new Sequence(10, 3, 1, 0, 2, 0);
 
+        // test set method
+        secondSequence.set(3, 10);
+
+        System.out.println("Range Test: should be: 10 times 0");
         System.out.println(firstSequence);
+        System.out.println();
+
+        System.out.println("Set test: should be 1, 2, 3, 10, 0, 0");
         System.out.println(secondSequence);
+        System.out.println();
+
+        System.out.println("Equal Test 1: should be false");
+        System.out.println(firstSequence.isEqual(secondSequence));
+        System.out.println();
+
+        System.out.println("Equal Test 1: should be false");
+        System.out.println(secondSequence.isEqual(thirdSequence));
+        System.out.println();
+
+        System.out.println("Same values Test: should be true");
+        System.out.println(thirdSequence.hasSameValues(fourthSequence));
+        System.out.println();
+
+        System.out.println("Permutation Test: should be true");
+        System.out.println(fourthSequence.isPermutationOf(fifthSequence));
+        System.out.println();
+
     }
 
 }
--- src/main/Sequence.java
+++ src/main/Sequence.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * This class is something like an {@link java.util.ArrayList}.
  */
@@ -26,22 +31,20 @@ public class Sequence {
     }
 
     /**
-     * Sets the given at a given position of the sequence;
+     * Sets the value given at a given position of the sequence;
      *
      * @param pos position, where to set the value
-     * @param val
+     * @param val value
      */
     public void set(int pos, int val) {
 
         // Make sure, the array is big enough
-        while (pos >= intArray.length) {
-            intArray = new int[intArray.length * 2];
+        if (pos >= intArray.length) {
+            intArray = Arrays.copyOf(intArray, intArray.length * 2);
         }
 
         // Set the value at the given position
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = val;
-        }
+        intArray[pos] = val;
     }
 
     /**
@@ -53,7 +56,7 @@ public class Sequence {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append('|');
         for (int i = 0; i < intArray.length; i++) {
-            stringBuilder.append(i);
+            stringBuilder.append(intArray[i]);
             if (i < intArray.length - 1) {
                 stringBuilder.append(' ');
             }
@@ -62,4 +65,91 @@ public class Sequence {
 
         return stringBuilder.toString();
     }
+
+    /**
+     * Checks, if the two sequences contain the same values in the same order.
+     *
+     * @param other object to compare with this instance
+     * @return
+     */
+    public boolean isEqual(Sequence other) {
+        return Arrays.equals(intArray, other.intArray);
+    }
+
+    /**
+     * Checks, if both sequences contain the same values, ignoring duplicates.
+     *
+     * @param other sequences, to campare this with
+     * @return
+     */
+    public boolean hasSameValues(Sequence other) {
+
+        // Remove duplicates and sort both arrays
+        int[] sortedValuesA = getSorted(removeDuplicates(intArray));
+        int[] sortedValuesB = getSorted(removeDuplicates(other.intArray));
+
+        return Arrays.equals(sortedValuesA, sortedValuesB);
+    }
+
+    /**
+     * Removes all duplicated values in the given array.
+     * No equals only ==
+     *
+     * @param array
+     * @return
+     */
+    private int[] removeDuplicates(int[] array) {
+        HashSet<Integer> intValuesA = new HashSet<Integer>(); // create hashSet from array
+
+        // add all values from the hashSet to the array
+        for (int i = 0; i < intArray.length; i++) {
+            intValuesA.add(intArray[i]);
+        }
+
+        // convert the hashSet into a int array
+        return intValuesA.stream().mapToInt(i -> i).toArray();
+    }
+
+    /**
+     * Sort the array and return it
+     *
+     * @param array
+     * @return
+     */
+    private int[] getSorted(int[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
+    public boolean isPermutationOf(Sequence other) {
+
+        boolean isPermutation = true;
+
+        // count all values in the array and store it in a hash map
+        HashMap<Integer, Integer> valueCounter = new HashMap<>();
+        for (int i = 0; i < intArray.length; i++) {
+            if (valueCounter.containsKey(intArray[i])) {
+                valueCounter.put(intArray[i], valueCounter.get(intArray[i]) + 1);
+            } else {
+                valueCounter.put(intArray[i], 1);
+            }
+        }
+
+        // remove all counts of the other array
+        for (int i = 0; i < other.intArray.length; i++) {
+            if (valueCounter.containsKey(other.intArray[i]) && valueCounter.get(other.intArray[i]) == 1) {
+                valueCounter.remove(intArray[i]);
+            } else if (valueCounter.containsKey(other.intArray[i]) && valueCounter.get(other.intArray[i]) > 1) {
+                valueCounter.put(other.intArray[i], valueCounter.get(other.intArray[i]) - 1);
+            } else {
+                isPermutation = false;
+            }
+            if (!valueCounter.isEmpty()) {
+                isPermutation = false;
+            }
+        }
+
+        return isPermutation;
+    }
+
 }
